0e07220ba80f13d5a98ced706d5e11d607d88e48
Sazerat <namulabre@gmail.com>
2022-06-27 02:02:08 +0200

    Ajout de commantaires un peu partout + refactor (ne change rien au fonctionnement du programme)

--- generateurNombreSpeudoAléatoire/generer_nombre.cpp
+++ generateurNombreSpeudoAléatoire/generer_nombre.cpp
@@ -1,12 +1,17 @@
 #include "generateur_speudo_aleatoire.h"
 // Je suis partie de là :
 //https://fr.wikipedia.org/wiki/G%C3%A9n%C3%A9rateur_congruentiel_lin%C3%A9aire
-//Le calcul effectuer est Xn+1 = a * X + c mod m, où X0 est la graine. Je tiens à préciser qu'étant donner que la fonction ne fait qu'une fois l'aupération, 
-//la variable "graine" ici représente surtout Xn-1.
+//Le calcul effectué est Xn+1 = a * X + c mod m, où X0 est la graine. Je tiens à préciser qu'étant donner que la fonction ne fait qu'une fois l'aupération, 
+//la variable "graine" ici représente est surtout Xn-1.
 
+/*
+	Cette fonction génère un nouveau nombre pseudo-aléatoire en utilisant un générateur.
+	Elle prend en paramètre un générateur défini (voir la fonction preparer_generateur)
+	Elle retourne ce même générateur, mais en changeant sa graine.
+*/
 GenerateurSpeudoAleatoire generer_nombre(GenerateurSpeudoAleatoire &generateur_speudo_aleatoire) {
-	int* pointeur_multiplicateur = &generateur_speudo_aleatoire.multiplicateur;
-	int* pointeur_additionneur = &generateur_speudo_aleatoire.additionneur;
+	const int* pointeur_multiplicateur = &generateur_speudo_aleatoire.multiplicateur;
+	const int* pointeur_additionneur = &generateur_speudo_aleatoire.additionneur;
 	int modulo = generateur_speudo_aleatoire.modulo;
 	
 	int* graine = &generateur_speudo_aleatoire.graine;
--- generateurNombreSpeudoAléatoire/preparer_generateur.cpp
+++ generateurNombreSpeudoAléatoire/preparer_generateur.cpp
@@ -4,18 +4,19 @@
 
 /*
 	Cette fonction prend en paramètre une graine, un modulo m, un multiplicateur a et un additionneur c
-	Elle retourne un generateur speudo aléatoire
-	Le but de cette fonction est de définir un générateur speudo aléatoire, une sorte de constructeur en somme.
+	Elle retourne un generateur pseudo-aléatoire
+	Le but de cette fonction est de définir un générateur pseudo-aléatoire, une sorte de constructeur en somme.
 	Cette fonction doit avoir tout ces paramètres différent de null pour fonctionner.
 */
-GenerateurSpeudoAleatoire preparer_generateur(int GRAINE, int m, int a, int c) {
+GenerateurSpeudoAleatoire preparer_generateur(int graine, int m, int a, int c) {
 	GenerateurSpeudoAleatoire* nouveau_generateur;
+	//On alloue la mémoire pour un nouveau générateur de nombre.
 	nouveau_generateur = (GenerateurSpeudoAleatoire*)malloc(sizeof(GenerateurSpeudoAleatoire));
-
-	nouveau_generateur->graine = GRAINE;
+	//On définie les attributs de la structures avec les entrées.
+	nouveau_generateur->graine = graine;
 	nouveau_generateur->additionneur = c;
 	nouveau_generateur->multiplicateur = a;
 	nouveau_generateur->modulo = m;
-
+	//On retourne le nouveau générateur
 	return *nouveau_generateur;
 }
